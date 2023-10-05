4bf6304b118c7026d42c6119f4acf7c979e1a243
40gilad <40gilad.shenkar@gmail.com>
2023-10-05 14:32:33 +0300

    server work started. rooms connection

---
+++ Backgamoon/Assets/Scripts/Menu/SC_MenuController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_MenuController : MonoBehaviour
+{
+    public void Btn_Play()
+    {
+        SC_MenuLogic.Instance.Btn_PlayLogic();
+    }
+}
